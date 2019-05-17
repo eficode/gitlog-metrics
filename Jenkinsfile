914ce53b57012b3269b27e1fb19c819041e62e33
Albert Rigo <alr@praqma.net>
2019-05-17 12:39:31 +0200

    use docker image instead of git repo for code-maat

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,25 +2,23 @@
 projectname="2git"
 startdate="2017-01-01"
 targetrepo="2git"
-codemaatfolder="code-maat"
 
 node('docker') {
   stage('checkout') {
     deleteDir()
     checkout scm
-    sh "git clone https://github.com/adamtornhill/code-maat.git ${codemaatfolder}"
     sh "git clone https://github.com/Praqma/2git.git ${targetrepo}"
   }
   stage('gather stats') {
-    sh "./getmetrics.sh ${projectname} ${startdate} ${targetrepo} ${codemaatfolder}"
+    sh "./getmetrics.sh ${projectname} ${startdate} ${targetrepo}"
   }
   stage('create plots') {
-      withEnv(["DATA=${projectname}"]) {
-        docker.image('drbosse/tidyverse-hashmap:0.1.1').inside {
-          sh '''ls -l
-          Rscript plot.r'''
-        }
+    withEnv(["DATA=${projectname}"]) {
+      docker.image('drbosse/tidyverse-hashmap:0.1.1').inside {
+        sh '''ls -l
+        Rscript plot.r'''
       }
+    }
   }
   stage('html') {
     sh "./html.sh ${projectname}"
--- getmetrics.sh
+++ getmetrics.sh
@@ -3,10 +3,9 @@
 projectname=$1
 startdate=$2
 targetrepo=$3
-codemaatfolder=$4
 
 if [ "$targetrepo" == "" ] ; then
-  echo "Error: not enough parameters, usage: $0 <project name> <start date> <path to repo> <code-maat path>"
+  echo "Error: not enough parameters, usage: $0 <project name> <start date> <path to repo>"
   exit
 fi
 
@@ -15,7 +14,6 @@ METRICFOLDER=$(pwd)
 TARGET=$METRICFOLDER/out/$projectname
 DATA=$TARGET/data
 CODESPACE=$METRICFOLDER/$targetrepo
-CODEMAAT=$METRICFOLDER/$codemaatfolder
 
 # Clean up previous runs
 rm $CODESPACE/logfile.log
@@ -24,18 +22,15 @@ mkdir -p $DATA
 
 # Start
 cd $CODESPACE
-git pull -pr
+#git pull -pr
 echo "./-\. Gathering metrics from $CODESPACE (after $startdate)"
 git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --no-merges --after=$startdate > $CODESPACE/logfile.log
 
-echo "./-\. Building CODE-MAAT"
-cd $CODEMAAT && docker build -t code-maat-app .
-
 echo "./-\. Generating authors-per-module.csv"
-docker run -v $CODESPACE:/data code-maat-app -l /data/logfile.log -c git2 > $DATA/authors-per-module.csv
+docker run -v $CODESPACE:/data kvarak/code-maat:0.1 -l /data/logfile.log -c git2 > $DATA/authors-per-module.csv
 for i in "coupling" "age" "abs-churn" "author-churn" "entity-churn" "entity-ownership" "entity-effort"; do
   echo "./-\. Generating $i.csv"
-  docker run -v $CODESPACE:/data code-maat-app -l /data/logfile.log -c git2 -a $i > $DATA/$i.csv
+  docker run -v $CODESPACE:/data kvarak/code-maat:0.1 -l /data/logfile.log -c git2 -a $i > $DATA/$i.csv
 done
 
 # post processing
